856229bf717ffd007d254ddcea0b0ad40185d1d2
Pedro Fagundes <pedrohefagundes@gmail.com>
2021-07-17 20:48:32 -0300

    deploy

---
+++ index.js
@@ -0,0 +1,119 @@
+function toAdventure1 () {
+document.getElementById('main-game-title').innerHTML = 'PORTAL';
+document.getElementById('wp').setAttribute("class", "portal-wp");
+document.getElementById('description').innerHTML = 'Portal™ is a new single player game from Valve. Set in the mysterious Aperture Science Laboratories, Portal has been called one of the most innovative new games on the horizon and will offer gamers hours of unique gameplay.';
+document.getElementById('play').setAttribute("href", "steam://store/400");
+document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/400/Portal/");
+}
+
+function toAdventure2 () {
+  document.getElementById('main-game-title').innerHTML = 'CRASH BANDICOOT';
+  document.getElementById('wp').setAttribute("class", "crash-wp");
+  document.getElementById('description').innerHTML = 'Your favorite marsupial, Crash Bandicoot™, is back! He’s enhanced, entranced and ready-to-dance with the N. Sane Trilogy. Relive all your favorite moments in Crash Bandicoot™, Crash Bandicoot™ 2: Cortex Strikes Back and Crash Bandicoot™ 3: Warped, now in fully-remastered graphical glory!';
+  document.getElementById('play').setAttribute("href", "steam://store/731490");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/731490/Crash_Bandicoot_N_Sane_Trilogy/");
+}
+
+function toAdventure3 () {
+  document.getElementById('main-game-title').innerHTML = 'DUST: AN ELYLISIAN TAIL';
+  document.getElementById('wp').setAttribute("class", "dust-wp");
+  document.getElementById('description').innerHTML = 'Immerse yourself in a gorgeous hand-painted world on a search for your true identity. As the mysterious warrior, Dust, your action-packed journey will take you from peaceful glades to snowy mountaintops and beyond.';
+  document.getElementById('play').setAttribute("href", "steam://store/236090");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/236090/Dust_An_Elysian_Tail/");
+}
+
+function toAdventure4 () {
+  document.getElementById('main-game-title').innerHTML = 'ORI AND THE BLIND FOREST';
+  document.getElementById('wp').setAttribute("class", "ori-wp");
+  document.getElementById('description').innerHTML = '“Ori and the Blind Forest” tells the tale of a young orphan destined for heroics, through a visually stunning action-platformer crafted by Moon Studios for PC.';
+  document.getElementById('play').setAttribute("href", "steam://store/261570");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/261570/Ori_and_the_Blind_Forest/");
+}
+
+function toAdventure5 () {
+  document.getElementById('main-game-title').innerHTML = 'LEGEND OF MANA';
+  document.getElementById('wp').setAttribute("class", "legendofmana-wp");
+  document.getElementById('description').innerHTML = 'The HD Remaster of Legend of Mana is coming to Steam. Meet a colorful cast of characters, use the unique "Land Make" System to build the World Map, and square off against fearsome monsters in the vibrant world of Fa’Diel.';
+  document.getElementById('play').setAttribute("href", "steam://store/1175830");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/1175830/Legend_of_Mana/");
+}
+
+function toRpg1() {
+  document.getElementById('main-game-title').innerHTML = 'CHILD OF LIGHT';
+  document.getElementById('wp').setAttribute("class", "childoflight-wp");
+  document.getElementById('description').innerHTML = 'Aurora, a young girl from 1895 Austria, awakens on the lost fairytale continent of Lemuria. To return home she must fight against the dark creatures of the Queen of the Night, who have stolen the sun, the moon and the stars. At stake is not only Lemuria, but Aurora’s true destiny.';
+  document.getElementById('play').setAttribute("href", "steam://store/256290");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/256290/Child_of_Light/");
+}
+
+function toRpg2() {
+  document.getElementById('main-game-title').innerHTML = 'FINAL FANTASY X | X2';
+  document.getElementById('wp').setAttribute("class", "ffx-wp");
+  document.getElementById('description').innerHTML = 'FINAL FANTASY X/X-2 HD Remaster celebrates two of the most cherished and beloved entries to the world-renowned franchise, completely remastered in gorgeous High Definition & now available on PC / Windows!';
+  document.getElementById('play').setAttribute("href", "steam://store/359870");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/359870/FINAL_FANTASY_XX2_HD_Remaster/");
+}
+
+function toRpg3() {
+  document.getElementById('main-game-title').innerHTML = 'TREE OF SAVIOR';
+  document.getElementById('wp').setAttribute("class", "tos-wp");
+  document.getElementById('description').innerHTML = 'Tree of Savior(abbreviated as TOS thereafter) is an MMORPG in which you embark on a journey to search for the goddesses in the world of chaos. Fairy-tale like colors accompanied with beautiful graphics in TOS will have you reminiscing about precious moments all throughout the game.';
+  document.getElementById('play').setAttribute("href", "steam://store/372000");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/372000/Tree_of_Savior_English_Ver/");
+}
+
+function toRpg4() {
+  document.getElementById('main-game-title').innerHTML = 'THE ELDER SCROLLS V: SKYRIM';
+  document.getElementById('wp').setAttribute("class", "skyrim-wp");
+  document.getElementById('description').innerHTML = 'Skyrim Special Edition brings the epic fantasy to life in stunning detail. The Special Edition includes the critically acclaimed game and add-ons with all-new features like remastered art and effects, volumetric god rays, dynamic depth of field, screen-space reflections, and more. Skyrim Special Edition also brings the full power of mods to the PC and consoles. New quests, environments, characters, dialogue, armor, weapons and more.';
+  document.getElementById('play').setAttribute("href", "steam://store/489830");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/489830/The_Elder_Scrolls_V_Skyrim_Special_Edition/");
+}
+
+function toRpg5() {
+  document.getElementById('main-game-title').innerHTML = 'THE WITCHER 3: WILD HUNT';
+  document.getElementById('wp').setAttribute("class", "witcher-wp");
+  document.getElementById('description').innerHTML = 'As war rages on throughout the Northern Realms, you take on the greatest contract of your life — tracking down the Child of Prophecy, a living weapon that can alter the shape of the world.';
+  document.getElementById('play').setAttribute("href", "steam://store/292030");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/");
+}
+
+function toStrategy1() {
+  document.getElementById('main-game-title').innerHTML = 'ORCS MUST DIE 2';
+  document.getElementById('wp').setAttribute("class", "omd2-wp");
+  document.getElementById('description').innerHTML = 'You’ve tossed, burned and sliced them by the thousands – now orcs must die more than ever before! Grab a friend and slay orcs in untold numbers in this sequel to the 2011 AIAS Strategy Game of the Year from Robot Entertainment.';
+  document.getElementById('play').setAttribute("href", "steam://store/201790");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/201790/Orcs_Must_Die_2/");
+}
+
+function toStrategy2() {
+  document.getElementById('main-game-title').innerHTML = 'AGE OF EMPIRES 3';
+  document.getElementById('wp').setAttribute("class", "aoe3-wp");
+  document.getElementById('description').innerHTML = 'Age of Empires III: Definitive Edition completes the celebration of one of the most beloved real-time strategy franchises in definitive form with enhanced features and modernized gameplay.';
+  document.getElementById('play').setAttribute("href", "steam://store/933110");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/933110/Age_of_Empires_III_Definitive_Edition/");
+}
+
+function toStrategy3() {
+  document.getElementById('main-game-title').innerHTML = 'PLANT VS ZOMBIES';
+  document.getElementById('wp').setAttribute("class", "pvz-wp");
+  document.getElementById('description').innerHTML = 'Zombies are invading your home, and the only defense is your arsenal of plants! Armed with an alien nursery-worth of zombie-zapping plants like peashooters and cherry bombs, you\'ll need to think fast and plant faster to stop dozens of types of zombies dead in their tracks.';
+  document.getElementById('play').setAttribute("href", "steam://store/3590");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/3590/Plants_vs_Zombies_GOTY_Edition/");
+}
+
+function toStrategy4() {
+  document.getElementById('main-game-title').innerHTML = 'VALIANT HEARTS';
+  document.getElementById('wp').setAttribute("class", "valianthearts-wp");
+  document.getElementById('description').innerHTML = 'Valiant Hearts : The Great War is the story of 4 crossed destinies and a broken love in a world torn apart. Dive into a 2D animated comic book adventure, mixing exploration, action and puzzles. Lost in the middle of the trenches, play each of these characters and help a young German soldier find his love!';
+  document.getElementById('play').setAttribute("href", "steam://store/260230");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/260230/Valiant_Hearts_The_Great_War__Soldats_Inconnus__Mmoires_de_la_Grande_Guerre/");
+}
+
+function toStrategy5() {
+  document.getElementById('main-game-title').innerHTML = 'XCOM 2';
+  document.getElementById('wp').setAttribute("class", "xcom2-wp");
+  document.getElementById('description').innerHTML = 'XCOM 2 is the sequel to XCOM: Enemy Unknown, the 2012 award-winning strategy game of the year. Earth has changed and is now under alien rule. Facing impossible odds you must rebuild XCOM, and ignite a global resistance to reclaim our world and save humanity.';
+  document.getElementById('play').setAttribute("href", "steam://store/268500");
+  document.getElementById('more-info').setAttribute("href", "https://store.steampowered.com/app/268500/XCOM_2/");
+}
